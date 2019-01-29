496f7ca4d4a70bae9ecc26f26e02bd0e49b12ea9
cloudycua <cloudycua@gmail.com>
2019-01-29 09:06:46 -0800

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -3,9 +3,29 @@
 function testFalseLooseEqualityComparison(){
     var x = 0;
     var y = 1;
-    if x == y { 
-        return true;
-    } else {
-        return false;
-    }
+    return x == y;
+}
+
+function testTrueLooseNonEqualityComparison(){
+    var x = 0;
+    var y = 1;
+    return x != y;
+}
+
+function testTrueLooseEqualityComparison(){
+    var x = 1;
+    var y = 1;
+    return x == y;
+}
+
+function testFalseStrictEqualityComparison(){
+    var x = 1;
+    var y = "1";
+    return x === y;
+}
+
+function testTrueStrictEqualityComparison(){
+    var x = 1;
+    var y = "1";
+    return x === parseInt(y);
 }
